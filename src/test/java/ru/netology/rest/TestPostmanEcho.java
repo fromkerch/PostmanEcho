4a77b68db36133e7e00d995ca0c18da2b6fc707e
Pavel Smirnov <luq2009@yandex.ru>
2021-02-12 15:02:53 +0300

    Added UTF-8 for contentType

--- src/test/java/ru/netology/rest/TestPostmanEcho.java
+++ src/test/java/ru/netology/rest/TestPostmanEcho.java
@@ -10,7 +10,7 @@ public class TestPostmanEcho {
     void shouldShowPostRequest() {
         given()
                 .baseUri("https://postman-echo.com")
-                
+                .contentType("text/plain; charset=UTF-8")
                 .body("Тестовые данные")
                 .when()
                 .post("/post")
